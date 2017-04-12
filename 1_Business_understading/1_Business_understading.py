fbe2d847633589a8fd0af855fe1cf60a39e1e834
G. Andrés Montoya <gleiwork@gmail.com>
2017-04-11 23:35:40 -0500

    data understanding

---
+++ 1_Business_understading/1_Business_understading.py
@@ -0,0 +1,38 @@
+
+# coding: utf-8
+
+# # 1. Business Understanding
+# ## 1.1 Determine Business Objectives
+# ### Outputs:
+# - Background
+# 
+# Ask a home buyer to describe their dream house, and they probably won't begin with the height of the basement ceiling or the proximity to an east-west railroad. But this playground competition's dataset proves that much more influences price negotiations than the number of bedrooms or a white-picket fence.
+# 
+# With 79 explanatory variables describing (almost) every aspect of residential homes in Ames, Iowa, this competition challenges you to predict the final price of each home.
+# 
+# - Business Objectives
+# 
+# To predict the final price of each home.
+# 
+# - Business Success
+# - Criteria
+# 
+
+# ## 1.2 Assess Situation
+# ### Outputs:
+# - Inventory of Resources 
+# - Requirements, Assumptions, and Constraints
+# - Risks and Contingencies
+# - Terminology
+# - Costs and Benefits
+# 
+# ## 1.3 Determine Data Mining Goals
+# ### Outputs:
+# - Data Mining Goals
+# - Data Mining Success Criteria
+# 
+# ## 1.4 Produce Project Plan
+# ### Outputs:
+# 
+# - Project Plan
+# - Initial Assessment of Tools and Techniques
--- 2_Data_understanding/2_Data_understading.py
+++ 2_Data_understanding/2_Data_understading.py
@@ -6,14 +6,52 @@
 # ### Outputs:
 # - Initial Data Collection Report
 # 
+# 
+
 # ## 2.2 Describe Data
 # ### Outputs:
 # - Data Description Report 
 # 
+# [Data description.txt](./data_description.txt)
+# 
+# 
+
 # ## 2.3 Explore Data
 # ### Outputs:
 # - Data Exploration Report
-# 
+
+# In[2]:
+
+import nltk
+import pandas as pd
+get_ipython().magic('matplotlib inline')
+import matplotlib
+import matplotlib.pyplot as plt
+from sklearn import datasets, linear_model
+import numpy as np
+
+
+# In[16]:
+
+train= pd.read_csv("../data/train.csv")
+train.set_index('Id')
+
+
+# In[20]:
+
+train['MSSubClass'].hist()
+
+
+# In[22]:
+
+train['MSZoning'].plot()
+
+
+# In[ ]:
+
+
+
+
 # ## 2.4 Verify Data Quality
 # ### Outputs:
 # 
